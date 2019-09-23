5b3ec074678b28681038397e9e2e63a23d806024
gbzhu <gbzhu@zju.edu.cn>
2019-09-23 18:55:22 +0800

    finish deploy

--- client2request_ml_fd/uaa_client.py
+++ client2request_ml_fd/uaa_client.py
@@ -10,12 +10,90 @@ from flask import Flask, globals, Response, request, g
 
 from bert import run_classifier, tokenization, optimization
 import numpy as np
-from config import MODEL_NAME, MODEL_SERVER_HOST, MODEL_SERVER_PORT, ROOT_CERT, LABELS_LIST, MAX_SEQ_LENGTH, VOCAB_FILE_PATH
+from config import MODEL_NAME, MODEL_SERVER_HOST, MODEL_SERVER_PORT, ROOT_CERT, LABELS_LIST, MAX_SEQ_LENGTH, \
+    VOCAB_FILE_PATH
+
+from sapjwt import jwtValidation
+from sap import xssec
+from functools import wraps
+from cfenv import AppEnv
+
 app = Flask(__name__)
+env = AppEnv()
+uaaCredentials = env.get_service(label='xsuaa').credentials
+
+
+@app.before_request
+def before_request():
+    g._uaaCredentials = uaaCredentials
+
+
+def parseJwt(tokenStr):
+    uaaCredentials = g._uaaCredentials
+    jwtValidator = jwtValidation()
+    # jwtValidator.setSecret(str(uaaCredentials['clientsecret']))
+    jwtValidator.loadPEM(str(uaaCredentials['verificationkey']))
+    result = jwtValidator.checkToken(tokenStr)
+    if (jwtValidator.getErrorDescription() != ""):
+        print("Error in JWT: {error}".format(error=jwtValidator.getErrorDescription()))
+    payload = jwtValidator.getPayload()
+    return json.loads(payload) if (payload != '') else {}
+
+
+def fetchUserToken(request):
+    """fetch user token"""
+    authHeader = request.headers.get('Authorization')
+    if authHeader is None:
+        return None
+
+    encodedJwtToken = authHeader.replace('Bearer ', '')
+    return parseJwt(str(encodedJwtToken))
+
+
+def validatejwt(encodedJwtToken):
+    """JWT offline validation"""
+    xs_security = getattr(g, '_sap_xssec', None)
+    if xs_security is None:
+        xs_security = xssec.create_security_context(encodedJwtToken, g._uaaCredentials)
+    if xs_security.get_grant_type is None:
+        return False
+
+    g._sap_xssec = xs_security
+    return True
+
+
+def checktoken():
+    """check JWT"""
+    authHeader = request.headers.get('Authorization')
+    if authHeader is None:
+        return False
+
+    encodedJwtToken = authHeader.replace('Bearer ', '').strip()
+    if encodedJwtToken == '':
+        return False
+
+    return validatejwt(encodedJwtToken)
+
+
+def sendauth():
+    """Sends a 403 response"""
+    return Response('Unauthorized', 401)
+
+
+def authenticated(func):
+    """ JWT token check decorator """
+
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if not checktoken():
+            return sendauth()
+
+        return func(*args, **kwargs)
+
+    return decorated
 
 
 def get_access_token():
-    print("******  begain to get access token  ******")
     url = "https://inno-demo.authentication.sap.hana.ondemand.com/oauth/token"
     querystring = {"grant_type": "client_credentials"}
     headers = {
@@ -24,11 +102,7 @@ def get_access_token():
     response = requests.request(
         "POST", url, headers=headers, params=querystring)
     if response.status_code == 200:
-        print("******   Done   *****")
         return 'Bearer ' + json.loads(response.text)['access_token']
-    else:
-        print("******   get token wrong   ******")
-        return
 
 
 def metadata_transformer(metadata):
@@ -39,6 +113,7 @@ def metadata_transformer(metadata):
 
 
 @app.route('/classify', methods=['POST', 'GET'])
+@authenticated
 def main():
     # request ml foundation to load model
     credentials = implementations.ssl_channel_credentials(
@@ -50,7 +125,6 @@ def main():
 
     # get the sentence of input
     sentence = str(globals.request.headers.getlist('Text')[0])
-    print(sentence)
 
     # convert single sentence to feature
     tokenizer = tokenization.FullTokenizer(
@@ -96,12 +170,10 @@ def main():
     #     shape.as_list())
     result_list = []
     for index in range(3):
-        result_list.append({"label":label_top3[index],"score":probabilities_top3[index]})
-    output_json = {"predictions":[{"result":result_list}]}
+        result_list.append({"label": label_top3[index], "score": str(probabilities_top3[index])})
+    output_json = {"predictions": [{"results": result_list}]}
     return Response(json.dumps(output_json), mimetype='application/json')
 
 
-port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-    app.debug = not os.getenv('PORT')
-    app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
